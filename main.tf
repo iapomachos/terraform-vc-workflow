346b2869a76d62f8cc28c57a05c28035243a8818
Ioannis Apomachos <144806418+iapomachos@users.noreply.github.com>
2025-03-06 08:43:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,4 @@ resource "azurerm_resource_group" "rg" {
   name     = random_pet.rg_name.id
 }
 
-resource "azurerm_resource_group" "rg2" {
-  location = var.resource_group_location
-  name     = random_pet.rg2_name.id
-}
-
 #test
